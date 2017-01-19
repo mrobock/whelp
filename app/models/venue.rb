50c4199c1c243358a3cca5c11ea1306908e62586
Chris Fasulo <fasulo810@gmail.com>
2017-01-18 16:02:22 -0800

    created the venues model and view, with testing

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,6 @@
 class Venue < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: true, length: { in: 5..20 }
+  validates :description, presence: true, length: { in: 10..500 }
 end
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,5 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe Venue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Venue" do
+    it "has to be real" do
+      expect{Venue.new}.to_not raise_error
+    end
+
+    it "can create and save a new venue" do
+      expect{Venue.new}.to_not raise_error
+      venue = Venue.new
+      venue.name = "Venue name"
+      venue.description = "venue description"
+      venue.street_1 = "123 venue ave"
+      venue.street_2 = "unit 1"
+      venue.city = "san diego"
+      venue.state = "CA"
+      venue.zip = "92108"
+      user1 = User.new(username: "Joe1", email: "joe@home.com", password: "password", password_confirmation: "password")
+      user1.save
+      venue.user = user1
+      expect{venue.save}.to_not raise_error
+    end
+
+    it "can read a current venue in the database" do
+      expect{Venue.new}.to_not raise_error
+      venue = Venue.new
+      venue.name = "Venue name"
+      venue.description = "venue description"
+      venue.street_1 = "123 venue ave"
+      venue.street_2 = "unit 1"
+      venue.city = "san diego"
+      venue.state = "CA"
+      venue.zip = "92108"
+      user1 = User.new(username: "Joe1", email: "joe@home.com", password: "password", password_confirmation: "password")
+      user1.save
+      venue.user = user1
+      expect{venue.save}.to_not raise_error
+
+      venue1 = Venue.find_by_name("Venue name")
+      expect(venue1.description).to eq("venue description")
+      expect(venue1.street_1).to eq("123 venue ave")
+      expect(venue1.street_2).to eq("unit 1")
+      expect(venue1.city).to eq("san diego")
+      expect(venue1.state).to eq("CA")
+      expect(venue1.zip).to eq("92108")
+      user2 = User.find_by_username("Joe1")
+      expect(user2.email).to eq("joe@home.com")
+    end
+
+    it "can modify a current venue in the database" do
+      expect{Venue.new}.to_not raise_error
+      venue = Venue.new
+      venue.name = "Venue name"
+      venue.description = "venue description"
+      venue.street_1 = "123 venue ave"
+      venue.street_2 = "unit 1"
+      venue.city = "san diego"
+      venue.state = "CA"
+      venue.zip = "92108"
+      user1 = User.new(username: "Joe1", email: "joe@home.com", password: "password", password_confirmation: "password")
+      user1.save
+      venue.user = user1
+      expect{venue.save}.to_not raise_error
+
+      venue1 = Venue.find_by_name("Venue name")
+      venue1.description = "venue update description"
+      venue1.save
+
+      venue2 = Venue.find_by_name("Venue name")
+      expect(venue1.description).to eq("venue update description")
+    end
+
+    it "can delete a current venue in the database" do
+      expect{Venue.new}.to_not raise_error
+      venue = Venue.new
+      venue.name = "Venue name"
+      venue.description = "venue description"
+      venue.street_1 = "123 venue ave"
+      venue.street_2 = "unit 1"
+      venue.city = "san diego"
+      venue.state = "CA"
+      venue.zip = "92108"
+      user1 = User.new(username: "Joe1", email: "joe@home.com", password: "password", password_confirmation: "password")
+      user1.save
+      venue.user = user1
+      expect{venue.save}.to_not raise_error
+
+      venue1 = Venue.find_by_name("Venue name")
+      expect{venue1.destroy}.to_not raise_error
+
+      expect(Venue.find_by_name("Venue name")).to be(nil)
+    end
+  end
 end
