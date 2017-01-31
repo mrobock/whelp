e4d4597835cca556364ac95012eec8d68b403acd
Max <mrobock@gmail.com>
2017-01-30 19:52:05 -0800

    some bug fixes (description scroll)

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,7 +13,7 @@ class Event < ApplicationRecord
   validates :name, presence: true
 
 #Adding paperclip
-  has_attached_file :image, styles: { small: "64x64#", med: "100x100#", large: "350x250#", xlarge: "400x400#" }, :default_url => "default_:style.png"
+  has_attached_file :image, styles: { small: "64x64#", med: "100x100#", large: "350x275#", xlarge: "400x400#" }, :default_url => "default_:style.png"
   validates_attachment :image,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
     size: { in: 0..10.megabytes }
